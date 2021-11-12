ff7fa5f12467538c011283c838ccd915e0d7c003
Drevers <mario.drevers@rwth-aachen.de>
2021-11-12 12:53:18 +0100

    Implemented the receipt class functions
    Implemented Receipt.generate_receipt() and Receipt.calculate_tax()
    Implemented Receipt.round() for the correct rounding process
    Fixed some spacing typos in the test strings
    Changed the names of the test cases to better represent their content

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import unittest
+import math
 from typing import Type
 
 class Item:
@@ -32,44 +33,66 @@ class Item:
         self.__imported_good == imported_good
 
 class Receipt:
-    __basic_sales_tax: float = 0.1
-    __import_tax: float = 0.05
-
     @staticmethod
     def generate_receipt(items:list):
-        raise NotImplementedError()
+        #generate the receipt string
+        receipt: str = ""
+        total_price: float = 0
+        total_tax: float = 0
+        for item in items:
+            item_tax: float = Receipt.calculate_tax(item)
+            total_tax += item_tax
+            item_price: float = item.get_amount()*item.get_price() + item_tax
+            total_price += item_price
+            receipt += str(item.get_amount()) + " " + str(item.get_name()) + ": " + f"{item_price:.2f}" + "\n"
+        receipt += "Sales Taxes: " + f"{total_tax:.2f}" + "\n"
+        receipt += "Total: " + f"{total_price:.2f}"
+        return receipt
 
     @staticmethod
     def calculate_tax(item:Type[Item]) -> float:
-        raise NotImplementedError()
+        #calculate the resulting tax for a given item
+        tax: float = 0.10
+        res: float = 0
+        if (item.is_exempt()):
+            tax = 0.00
+        if (item.is_imported()):
+            tax += 0.05
+        res = item.get_amount()*item.get_price()*tax
+        return Receipt.round(res)
+
+    @staticmethod
+    def round(num:float) -> float:
+        #round the given number in accordance to the challenge's rules
+        return math.ceil(num*20.00)/20.00
 
 class TestReceipt(unittest.TestCase):
 
-    def test_exempt_goods(self):
-        #test if the code can handle exempt goods
+    def test_normal_exempt_goods(self):
+        #test if the code can handle normal and exempt goods
         book:Type[Item] = Item("book",12.49,1,True,False)
-        cd:Type[Item] = Item("music cd",14.99,1,True,False)
+        cd:Type[Item] = Item("music CD",14.99,1,False,False)
         chocolate:Type[Item] = Item("chocolate bar",0.85,1,True,False)
         goods:list = [book,cd,chocolate]
-        res = "1 book: 12.49\n 1 music CD: 16.49\n 1 chocolate bar: 0.85\n Sales Taxes: 1.50\n Total: 29.83"
+        res = "1 book: 12.49\n1 music CD: 16.49\n1 chocolate bar: 0.85\nSales Taxes: 1.50\nTotal: 29.83"
         self.assertEqual(res,Receipt.generate_receipt(goods))
 
-    def test_imported_goods(self):
-        #test if the code can handle imported goods
+    def test_exempt_imported_goods(self):
+        #test if the code can handle exepmt and imported goods
         chocolate:Type[Item] = Item("imported box of chocolates",10.00,1,True,True)
         perfume:Type[Item] = Item("imported bottle of perfume",47.50,1,False,True)
         goods:list = [chocolate,perfume]
-        res = "1 imported box of chocolates: 10.50\n 1 imported bottle of perfume: 54.65\n Sales Taxes: 7.65\n Total: 65.15"
+        res = "1 imported box of chocolates: 10.50\n1 imported bottle of perfume: 54.65\nSales Taxes: 7.65\nTotal: 65.15"
         self.assertEqual(res,Receipt.generate_receipt(goods))
 
-    def test_mixed_goods(self):
-        #test if the code can handle mixed goods
+    def test_normal_exempt_imported_goods(self):
+        #test if the code can handle normal, exempt and imported goods
         perfume_imported:Type[Item] = Item("imported bottle of perfume",27.99,1,False,True)
-        perfume:Type[Item] = Item("bottle of perfume",18.99,1,False,True)
+        perfume:Type[Item] = Item("bottle of perfume",18.99,1,False,False)
         pills:Type[Item] = Item("packet of headache pills",9.75,1,True,False)
         chocolate:Type[Item] = Item("imported box of chocolates",11.25,1,True,True)
         goods:list = [perfume_imported,perfume,pills,chocolate]
-        res = "1 imported bottle of perfume: 32.19\n 1 bottle of perfume: 20.89\n 1 packet of headache pills: 9.75\n 1 imported box of chocolates: 11.85\n Sales Taxes: 6.70\n Total: 74.68"
+        res = "1 imported bottle of perfume: 32.19\n1 bottle of perfume: 20.89\n1 packet of headache pills: 9.75\n1 imported box of chocolates: 11.85\nSales Taxes: 6.70\nTotal: 74.68"
         self.assertEqual(res,Receipt.generate_receipt(goods))
 
 
